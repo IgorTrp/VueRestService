94115eeb1a30db8baed529591769967dea5d9168
IgorTrp <itrpkovic12920rn@raf.rs>
2022-08-29 13:08:38 +0200

    dawd

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  HOST: "localhost",
-  PORT: "1433",
-  USER: "Igor",
-  PASSWORD: "bazapodataka123",
-  DB: "SkritBaza",
-  dialect: "mssql",
+  HOST: "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
+  PORT: "5432",
+  USER: "bekttbbiftwvqn",
+  PASSWORD: "ab14fbe449d2550913f99bbb23104385b55878e785fa15e45552958ef8da6e46",
+  DB: "d8kg218b8se9t",
+  dialect: "postgres",
   dialectOptions:{
 	ssl:{
 		require:true,
