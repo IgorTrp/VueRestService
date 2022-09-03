8c5cd133551acb14602ae66bd51d33dca70c2adc
IgorTrp <itrpkovic12920rn@raf.rs>
2022-09-04 00:18:06 +0200

    Update tnjitovi.js

--- tnjitovi.js
+++ tnjitovi.js
@@ -57,12 +57,12 @@ ruter.use(ekspres.urlencoded({ extended: true }));
 //GET
 ruter.get("/", async(req,res)=>{
   if(isNaN((parseInt(req.query.id)))){
-  db.sequelize.query('SELECT KorisnikId,KorisnickoIme,Sadrzaj,Goreglasovi,Doleglasovi,Datum FROM Komentar LEFT JOIN Korisnik ON Komentar.KorisnikId = Korisnik.Id ORDER BY Datum Desc LIMIT 100')
+  db.sequelize.query('SELECT KorisnickoIme,Sadrzaj,COUNT(o.goreglasovi) AS gore,COUNT(o.doleglasovi) AS dole,Datum FROM komentar LEFT JOIN korisnik AS kr ON komentar.KorisnikId = kr.Id LEFT JOIN ocena AS o ON komentar.id=o.komentarid GROUP BY komentar.id,komentar.korisnikid, komentar.korisnikid,kr.KorisnickoIme,komentar.Sadrzaj,komentar.datum ORDER BY Datum DESC  LIMIT 100')
   .then(function(result) {res.status(200).send(result);})
   .catch( err => res.status(500).json(err) );
   }
   else{
-    db.sequelize.query('SELECT KorisnikId,KorisnickoIme,Sadrzaj,Goreglasovi,Doleglasovi,Datum FROM Komentar LEFT JOIN Korisnik ON Komentar.KorisnikId = Korisnik.Id WHERE Komentar.KorisnikId ='+req.query.id+' ORDER BY Datum Desc')
+    db.sequelize.query("SELECT KorisnickoIme,Sadrzaj,COUNT(o.goreglasovi) AS gore,COUNT(o.doleglasovi) AS dole,Datum FROM komentar LEFT JOIN korisnik AS kr ON komentar.KorisnikId = kr.Id LEFT JOIN ocena AS o ON komentar.id=o.komentarid WHERE komentar.id ="+req.query.id+" GROUP BY komentar.id,komentar.korisnikid, komentar.korisnikid,kr.KorisnickoIme,komentar.Sadrzaj,komentar.datum ORDER BY Datum DESC  LIMIT 100")
     .then(function(result) {res.status(200).send(result);})
     .catch( err => res.status(500).json(err) );
   }
